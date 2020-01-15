1d0db775291fde658d51c9166e339c9a903d4d0e
wnminch <59751231+wnminch@users.noreply.github.com>
2020-01-14 18:41:26 -0800

    Add files via upload

---
+++ historical_lux.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+
+import time
+import os
+
+
+if __name__ == "__main__":
+	os.system('touch lux_values.txt')
+	for i in range(10):	
+		output = os.system('cat /tmp/last_lux_value')
+		os.system(f'echo "{time.ctime()} lux_value: {output}" >> lux_values.txt')
+		time.sleep(30)
+		
+
