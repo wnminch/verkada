f2cc42f526410ffdef2a1f40c415bb5824190aaa
wnminch <59751231+wnminch@users.noreply.github.com>
2020-01-14 19:44:40 -0800

    Update historical_lux.py

--- historical_lux.py
+++ historical_lux.py
@@ -6,10 +6,13 @@ import os
 
 
 if __name__ == "__main__":
+	"""Create text file of lux values overnight"""
 	os.system('touch /mnt/ramdisk/lux_values.txt')
-	for i in range(10):	
+	# currently set up to take 16 measurements over the next 8 hours
+	for i in range(16):	
 		output = os.system('cat /tmp/last_lux_value')
-		os.system('echo -e "' + time.ctime() + 'lux_value: ' + str(output) + '\n" >> /mnt/ramdisk/lux_values.txt')
-		time.sleep(5)
+		os.system('echo -e "' + time.ctime() + ' lux_value: ' + str(output) + '\n" >> /mnt/ramdisk/lux_values.txt')
+		# wait 30 minutes before next iteration 
+		time.sleep(60*30)
 		
 # curl -L https://raw.githubusercontent.com/wnminch/verkada/master/historical_lux.py > /mnt/ramdisk/historical_lux.py
