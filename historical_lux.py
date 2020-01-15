cd9497b3897fb75678443bc8becfb876128d3975
wnminch <59751231+wnminch@users.noreply.github.com>
2020-01-14 19:17:23 -0800

    Update historical_lux.py

--- historical_lux.py
+++ historical_lux.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	os.system('touch lux_values.txt')
 	for i in range(10):	
 		output = os.system('cat /tmp/last_lux_value')
-		os.system('echo -e "' + time.ctime() + 'lux_value: ' + output + '\n" >> lux_values.txt')
+		os.system('echo -e "' + str(time.ctime()) + 'lux_value: ' + output + '\n" >> lux_values.txt')
 		time.sleep(30)
 		
 # curl -L https://raw.githubusercontent.com/wnminch/verkada/master/historical_lux.py > /mnt/ramdisk/historical_lux.py
