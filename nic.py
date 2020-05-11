c81378213d63e9a621b09e59d9c9c207ea6ab983
wnminch <59751231+wnminch@users.noreply.github.com>
2020-05-10 19:01:47 -0700

    Update nic.py

--- nic.py
+++ nic.py
@@ -4,16 +4,16 @@ import time
 import os
 
 if __name__ == "__main__":
-    """Collect ifconfig and route repeatedly"""
+    """Collect ifconfig and route hourly for 2 days"""
 
     os.system('touch /mnt/ramdisk/nic.txt')
 
-    for i in range(4):
-        os.system('echo -e "\n\n' + time.ctime() +'" >> /mnt/ramdisk/nic.txt')
+    for i in range(48):
+        os.system('echo -e "\n\n' + time.ctime() + '" >> /mnt/ramdisk/nic.txt')
         ifconfig = os.system('ifconfig | grep . >> /mnt/ramdisk/nic.txt')
         route = os.system('route | grep . >> /mnt/ramdisk/nic.txt')
-        time.sleep(30)
+        time.sleep(60*60)
 
 # curl -L https://raw.githubusercontent.com/wnminch/verkada/master/nic.py > /mnt/ramdisk/nic.py
-# python3 /mnt/ramdisk/nic.py >> /dev/null
+# nice python3 /mnt/ramdisk/nic.py >> /dev/null
 # cat /mnt/ramdisk/nic.txt
