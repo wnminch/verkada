584a8819c9af9e0f04771d9846f148e53a60537a
wnminch <59751231+wnminch@users.noreply.github.com>
2020-06-04 09:21:38 -0700

    Add files via upload

---
+++ PingMain (1).py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+
+import time
+import os
+
+if __name__ == "__main__":
+    """Ping 4 times 2 endpoints every 10 min"""
+    os.system('touch /mnt/internal/mmcblk0p7/gwtest.txt')
+    os.system('touch /mnt/internal/mmcblk0p7/pingtest.txt')
+    os.system('touch /mnt/internal/mmcblk0p7/dnstest.txt')
+    i = 0
+
+    while i < 1008:
+        os.system("date |  tee -a /mnt/internal/mmcblk0p7/gwtest.txt ;ping -c4 -w10 192.168.1.1 |  tee -a /mnt/internal/mmcblk0p7/gwtest.txt")
+        os.system("date |  tee -a /mnt/internal/mmcblk0p7/pingtest.txt ;ping -c4 -w10 8.8.8.8 |  tee -a /mnt/internal/mmcblk0p7/pingtest.txt")
+        os.system("date |  tee -a /mnt/internal/mmcblk0p7/dnstest.txt ;ping -c4 -w10 google.com |  tee -a /mnt/internal/mmcblk0p7/dnstest.txt")
+
+        i += 1
+        time.sleep(60 * 2)
+
